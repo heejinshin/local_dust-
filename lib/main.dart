954a0da6286c5527e2556cac7f8247e95502b6a0
heejinshin <heejinshin02@gmail.com>
2023-03-07 00:39:49 +0900

    api completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'models/AirResult.dart';
+
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 void main() {
   runApp(const MyApp());
 }
@@ -12,63 +17,156 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
-      home:  Main(),
+      home: Main(),
     );
   }
 }
 
 class Main extends StatefulWidget {
-
   @override
   State<Main> createState() => _MainState();
 }
 
 class _MainState extends State<Main> {
+  AirResult? _result;
+
+  Future<AirResult> fetchData() async {
+    var response = await http.get(Uri.parse('http://api.airvisual'
+        '.com/v2/nearest_city?key=e0d3bdef-758b-4d13-96a9-08c0a4aba760'));
+
+    AirResult result = AirResult.fromJson(json.decode(response.body));
+    return result;
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    fetchData().then((airResult) {
+      setState(() {
+        _result = airResult;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-          Text('현재 위치 미세먼지', style: TextStyle(fontSize: 30),),
-          Card(
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Text('얼굴사진'),
-                  Text('80'),
-                  Text('보통'),
-                ],
-              ),
-              Row(
-                children: [
-                  Text('11'),
-                  Text('습도'),
-                  Text('풍속'),
-                ],
-              ),
-            ],
-          ),
-      ),
-          ClipRRect(  
-            borderRadius: BorderRadius.circular(30),
-            child: ElevatedButton(
+        child: _result == null
+            ? CircularProgressIndicator()
+            : Padding(
+                padding: const EdgeInsets.all(7.0),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        '현재 위치 미세먼지',
+                        style: TextStyle(fontSize: 30),
+                      ),
+                      SizedBox(height: 16),
+                      Card(
+                        child: Column(
+                          children: [
+                            Container(
+                              color:  getColor(_result),
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  Text('얼굴사진'),
+                                  Text(
+                                    '${_result?.data?.current?.pollution?.aqius}',
+                                    style: TextStyle(fontSize: 40),
+                                  ),
+                                  Text(
+                                    getString(_result),
+                                    style: TextStyle(fontSize: 20),
+                                  ),
+                                ],
+                              ),
 
-              onPressed: (){},
-              child: Icon(Icons.refresh, color: Colors.white,),
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.orange,
-                  padding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 50)
+                            ),
+
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Row(
+                                    children: <Widget>[
+                                      Image.network('https://airvisual.com/images/${_result?.data?.current?.weather?.ic}.png', width: 32,height: 32,),
+
+                                      SizedBox(
+                                        width: 16,
+                                      ),
+                                      Text(
+                                        '${_result?.data?.current?.weather?.tp}',
+                                        style: TextStyle(fontSize: 16),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Text(
+                                    '습도 ${_result?.data?.current?.weather?.hu}%'),
+                                Text(
+                                    '풍속 ${_result?.data?.current?.weather?.ws}m/s'),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 16,
+                      ),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(30),
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: Icon(
+                            Icons.refresh,
+                            color: Colors.white,
+                          ),
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.orange,
+                              padding: const EdgeInsets.symmetric(
+                                  vertical: 15.0, horizontal: 50)),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
               ),
-            ),
-          )
-        ],),
       ),
     );
+  }
+
+  Color getColor(AirResult? result) {
+      if(result!.data!.current!.pollution!.aqius !=null && result.data!.current!.pollution!.aqius! <= 50) {
+        return Colors.greenAccent;
+      }else if(result.data!.current!.pollution!.aqius !=null && result.data!.current!.pollution!.aqius! <= 100)  {
+        return Colors.orange;
+      }else if(result.data!.current!.pollution!.aqius !=null && result.data!.current!.pollution!.aqius! <= 150)  {
+        return Colors.deepOrange;
+      }else {
+        return Colors.red;
+      }
+  }
+
+  String getString(AirResult? result) {
+    if(result!.data!.current!.pollution!.aqius !=null && result.data!.current!.pollution!.aqius! <= 50) {
+      return '좋음';
+    }else if(result.data!.current!.pollution!.aqius !=null && result.data!.current!.pollution!.aqius! <= 100)  {
+      return '보통';
+    }else if(result.data!.current!.pollution!.aqius !=null && result.data!.current!.pollution!.aqius! <= 150)  {
+      return '나쁨';
+    }else {
+      return '아주나쁨';
+    }
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,22 +9,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:local_dust/main.dart';
+import 'package:http/http.dart' as http;
+import 'package:local_dust/models/AirResult.dart';
+
+import 'dart:convert';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  test('http 통신 테스트', () async{
+    var response = await http.get(Uri.parse('http://api.airvisual'
+        '.com/v2/nearest_city?key=e0d3bdef-758b-4d13-96a9-08c0a4aba760'));
+    expect(response.statusCode, 200);
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    AirResult result = AirResult.fromJson(json.decode(response.body));
+    expect(result.status, 'success');
   });
+
 }
+
+
